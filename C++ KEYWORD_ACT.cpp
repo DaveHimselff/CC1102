93a4559ded27955fc22d570db43602e42f51759e
Dave_Kelsy <114577954+DaveKelsy@users.noreply.github.com>
2022-10-19 10:54:50 +0800

    Add files via upload

---
+++ C++ KEYWORD_ACT.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	int x = 88;
+	double d = 40;
+	cout << "int x = 88" << endl;
+	cout << "double d =40" << endl;
+	cout << endl;
+	cout << endl;
+	cout << x + d << endl;
+	cout << x - d << endl;
+	cout << x * d << endl;
+	cout << x / d << endl;
+
+}
