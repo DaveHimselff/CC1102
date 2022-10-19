62d7a38e7326ea4277a10bcd89826b5f715cca53
Dave_Kelsy <114577954+DaveKelsy@users.noreply.github.com>
2022-10-19 10:50:35 +0800

    Add files via upload

---
+++ 2ND_ACT.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int numbers = 5000, secondnumber = 6500, thirdnumber = 8000;
+	cout << "The following names shows their income last month by the following:" << "simon_got" << "=" << numbers << "," << "dave_got" << "=" << secondnumber << "_" << "and_lastly" << "_" << "jean_got" << "=" << thirdnumber << endl;
+	cout << endl;
+}
